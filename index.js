5d484a52248becebed2aa4fe5c63301c378ad2de
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-20 14:05:03 +0300

    if-else-if

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
+/* if - else if */
 
-function getValueFromUser() {
-    let userName = prompt('Як Вас звати?');
-    sayHello(userName);
-}
+const userAge = Number(prompt('How old are you?'));
 
-
-function sayHello(abracadabra){
-    debugger;
-    console.log('Hello ' + abracadabra);
-}
-
-getValueFromUser();
-
-
-/*
-
-Написати функцію, яка приймає число і якщо воно парне, повертає true, в іншому випадку повертає false
-
-*/
-
-
-function isEven(number){
-    return number % 2 === 0
+if(userAge > 40) {
+    console.log('Your age is bigger than 40')
+} else if (userAge > 20 && userAge < 40) {
+    console.log('Your are in 20 to 40');
+} else {
+    console.log('else')
 }
