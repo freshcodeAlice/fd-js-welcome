756a76564825c5de4e5f3fd813c870ed8b4fe4f1
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-21 14:52:10 +0300

    while, do-while

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
-/*
-const userAvatar = 'http://.....';
-const imagePlaceholder = './';
+// console.log('1');
 
 
+// while (умова = true) {
+// блок коду, який буде виконуватись, поки умова = true
+// }
 
-let imageSrc = userAvatar ? userAvatar : imagePlaceholder;
-<img src="" />
-*/
-
-
-/*
-Спитайте у юзера, як його звуть, і виведіть на консоль "Hello " + userName
-Якщо користувач нічого не ввів - виведіть "Hello, Anonym"
-
-*/
-
-const userName = prompt('What is your name?');
-
-function sayHello(name = 'Anonym'){
-    console.log('Hello ' + name);
+let i = 0;
+while (i < 10) {
+    i++;
+    console.log('1');
 }
 
 
-sayHello('Alex');
+// do {
+//    блок коду
+// } while(умова)
+
+let j = 0;
+do {
+    console.log('2')
+} while (j++ < 3);
 
 
-sayHello('John');
+/*
+Задача. 
+Маємо змінну з рядком 'GoodPassword'
+ Запитайте у юзера через prompt пароль, порівняйте його з цим рядком, якщо користувач ввів неправильний пароль - запитайте ще раз. Якщо правильний - виведіть на консоль "Пароль правильний"
 
-sayHello(userName);
 
-sayHello();
+*/
+
+const password = 'GoodPassword';
+  
+
+let userValue = '';
+do {
+//    userValue = prompt('Type your password');
+} while (userValue !== password);
+console.log('Password correct');
+
+
+
+/*
+Вивести на консоль всі парні числа від 1 до 10
+
+
+*/
+let n = 1;
+while (n++ < 10) {
+    if ( n % 2 === 0 ) {
+        console.log(n);
+    }
+}
