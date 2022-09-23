55c2945f006253a0c4c25f3624e8befaff600c0b
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-23 16:04:49 +0300

    User task resolve

--- index.js
+++ index.js
@@ -19,19 +19,19 @@
 //     }
 // }
 
-function User(firstName, lastName) {
-    // {}, кладе його у this
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = 20;
-    this.sayHello = function() {
-        console.log(this.firstName + ' say: "Hello"');
-    }
-}
+// function User(firstName, lastName) {
+//     // {}, кладе його у this
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = 20;
+//     this.sayHello = function() {
+//         console.log(this.firstName + ' say: "Hello"');
+//     }
+// }
 
 
-const us = new User('Jack', 'Sparrow');
-us.firstName;
+// const us = new User('Jack', 'Sparrow');
+// us.firstName;
 
 
 /*
@@ -85,5 +85,43 @@ function Car(brand, model, engineVolume, seatsQuantity, maxSpeed) {
             return this.stop();
         }
         return this.run();
+    }
+}
+
+
+/*
+Функція-конструктор User.
+Юзер має:
+- ім'я
+- прізвище
+- вік
+- мейл
+- пароль
+- залогінений (isLogIn) 
+
+Методи:
+- повне ім'я - повертає рядок з іменем і прізвищем
+
+- signIn - приймає пароль, порівнюємо його з паролем, який зберігаємо, якщо вони співпадають - користувач буде залогінений. Якщо ні, відповідаємо "неправильний пароль"
+
+*/
+
+function User(firstName, lastName, age, email, password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.email = email;
+    this.password = password;
+    this.isLogIn = false;
+    this.getFullName = function () {
+        return this.firstName + ' ' + this.lastName;
+    }
+    this.signIn = function(pass) {
+        if( pass === this.password) {
+            this.isLogIn = true;
+            return 'You are logged in'
+        } else {
+            return 'Password incorrect'
+        }
     }
 }
