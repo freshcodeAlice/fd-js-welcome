de00010e72017005eb8dfce0ead4e880552ed270
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-26 17:01:45 +0300

    map

--- index.js
+++ index.js
@@ -7,4 +7,22 @@ function printElement(num, i) {
 
 const arr = [2, 3, 1, 2, 4, 2, 1, 2];
 
-arr.forEach(printElement);
+//arr.forEach(printElement);
+
+
+
+
+//////
+
+function multyTwo(num) {
+    return num*2
+}
+
+// const resultArray = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     let element = multyTwo(arr[i]);
+//     resultArray.push(element);
+// }
+
+const resultArray = arr.map(multyTwo);
