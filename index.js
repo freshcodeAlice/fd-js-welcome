28d9948302bdcc1eca34f93798b5af25fa24406c
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-27 15:42:20 +0300

    sumOfAllArguments

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 'use strict'
 
-/*
-
-Реалізуйте метод map
-
-
-*/
-
-const arrNative = [2, 3, 4, 5];
-
-
-function multyElem(num) {
-    return num*2;
+function sumOfAllArguments() {
+    let sum = 0;
+        for (let i = 0; i < arguments.length; i++) {
+            sum += arguments[i];    
+        }
+  return sum;
 }
 
-///[4, 6, 8, 10];
-
-const arr = [];
-for (let i = 0; i < arrNative.length; i++) {
-    arr.push(multyElem(arrNative[i]));
-}
-
-console.log(arr);
