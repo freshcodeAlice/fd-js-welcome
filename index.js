f309cbba7893b869b4f0ffc6394c2cc55c4b1f73
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-29 14:10:06 +0300

    arrowFunction

--- index.js
+++ index.js
@@ -1,125 +1,26 @@
 'use strict'
 
-const NATIONALITIES = [
-    'Ukraine',
-    'Great Britain',
-    'Germany',
-    'France',
-    'Spain'
-];
-
-/*
-Створити функцію-конструктор для об'єкта User.
-User має властивості:
-- firstName
-- lastName
-- age
-- mail
-- isSubscribe
-- nationality
-
-*/
-
-function User(firstName, lastName, age, mail, isSubscribe = false, nationality){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.mail = mail;
-    this.isSubscribe = isSubscribe;
-    this.nationality = nationality;
+const obj = {
+    key: 'value',
+    testMethod: test
 }
 
-/*
-Створити функцію, яка створює 1 користувача.
-1. Відбудовує об'єкт Юзера, передає йому дані
-`Name 1`, `LastName 1`, 20, `mail1@com`, true
-2. Повернути об'єкт користувача
-
-*/
-
-
-function getRandomArbitrary(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
-
-function createArrayOfUsers(quantity) {
-    const userArr = [];
-    for (let i = 0; i < quantity; i++) {
-        const us = new User(`Name ${i}`, `LastName ${i}`, getRandomArbitrary(1, 100), `mail${i}@com`, Boolean(Math.round(Math.random())), NATIONALITIES[getRandomArbitrary(0, 4)])
-        userArr.push(us);
-    }
-   
-    return userArr;
+function test() {
+    console.log(this);
 }
 
 
-const arrUsers = createArrayOfUsers(50); // [] (50)
-//console.table(arrUsers);
+const test2 = function () {
+    console.log(this);
+}
 
-/* 
-Отримати масив повних імен юзерів 
-*/
-
-
-const fNameArray = arrUsers.map(function(userObj) {
-   return `${userObj.firstName} ${userObj.lastName}`;
-});
-
-arrUsers.sort(function (usA, usB) {
-    return usA.age - usB.age;
-})
-
-console.table(arrUsers);
+const arrowFunction = a => a*a;
 
 
-/* filter */
+obj.testMethod();
+test();
+test2();
+arrowFunction();
 
 
-/*
-Отримати масив всіх повнолітніх користувачів, які підписані на новини
-
-*/
-
-const filtered = arrUsers.filter(function (elem) {
-    return elem.age > 20 && elem.isSubscribe
-})
-
-
-/*
-Отримати масив емейлів всіх повнолітніх користувачів, які підписані
-
-*/
-
-const mail = arrUsers
-                .filter(function (elem) {
-                    return elem.age > 18 && elem.isSubscribe
-                }).map(function(user){
-                    return user.mail
-                })
-
-/*
-Зробити масив з повних імен (ім'я + прізвище) користувачів з України, які підписані на наші новини
-
-*/
-
-const fullNamesOfUkrainian = arrUsers
-                                .filter( function (user) {
-                                    return (user.nationality === 'Ukraine') && user.isSubscribe
-                                })
-                                .map( function (user){
-                                    return `${user.firstName} ${user.lastName}`
-                                })
-
-/* forEach */
-
-/*
-Підпишіть на новини ВСІХ користувачів старше 18 років
-*/
-
-arrUsers.forEach(function(user){
-    if(user.age >= 18) {
-        user.isSubscribe = true;
-    }
-})
-
-console.table(arrUsers)
+console.log(this);
