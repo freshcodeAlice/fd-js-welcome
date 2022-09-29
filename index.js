c5eb78a07d9a94beea82caab80f723da907de4a2
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-29 16:05:20 +0300

    Triangle star task

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
  'use strict'
 
-for(let i = 1; i < 5; i++) {
-    for (let j = 1; j < 5; j++) {
-        debugger
-        console.log(`${j} * ${i} = ${j*i}`);
+// for(let i = 1; i < 5; i++) {
+//     for (let j = 1; j < 5; j++) {
+//         console.log(`${j} * ${i} = ${j*i}`);
+//     }
+//  console.log('-----------------');
+// }
+
+
+/*
+
+Намалювати в консолі прямокутний трикутник висотою в 8 рядків
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+
+*/
+let stars = '';
+for (let i=1; i<8; i++) {
+        for (let j = 0; j < i; j++) {
+        stars += '*';
+        }
+        stars += '\n';
     }
- console.log('-----------------');
-}
+
+    console.log(stars);
