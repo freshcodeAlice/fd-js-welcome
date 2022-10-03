799e0e6616e8467bfff82b22788607d9e0373506
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-03 16:44:11 +0300

    Fibonacchi task (resolve, but not ideal)

--- index.js
+++ index.js
@@ -66,3 +66,30 @@ console.log(sumOfArrays(arr));
 const arr2 = [5, 1, 3, 4, [5, 3, 2, [2, 10]]];
 
 console.log(sumOfArrays(arr2));
+
+
+
+/*
+Вивести на консоль перші 100 членів ряду Фібоначчі
+
+F(n) = F(n-1) + F(n-2)
+
+F(1) = 1
+F(2) = 1
+
+*/
+
+
+function fn(n) {
+        if(n === 0 ) {
+                return 0;
+        }
+        if(n===2 || n===1) {
+                return 1
+        }
+        return fn(n-1) + fn(n-2)
+}
+
+for (let i = 0; i<10; i++) {
+        console.log(fn(i));
+}
