706106f3931b44b2a7001634fbdcdea18a0004af
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-04 15:40:18 +0300

    Auto + Fuel task resolve

---
+++ src/new_syntax.js
@@ -0,0 +1,38 @@
+class MyNewArray {
+    constructor() {
+        this.length = 0;
+    }
+
+    push() {
+        for (let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
+        }
+        return this.length;
+    }
+
+    pop() {
+        const lastItem = this[this.length-1];
+        delete this[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+
+    forEach(callback) {
+        for (let i = 0; i < this.length; i++) {
+            callback(this[i], i, this);
+        }
+    }
+
+    map(fn) {
+        const arr = new MyArray();
+        for (let i = 0; i < this.length; i++) {
+            arr.push(fn(this[i]));
+        }
+        return arr;
+    }
+
+}
+
+
+const mnarr = new MyNewArray();
