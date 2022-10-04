30c390452b62032f6a3a8b9396dfeba0ef209de3
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-04 14:35:01 +0300

    Throw Errors

--- index.js
+++ index.js
@@ -7,5 +7,23 @@
   */
 
  function sayHello(name) {
+       if (typeof name !== 'string') {
+              throw new TypeError('Name must be a string');
+       }
         console.log(`Hello, ${name}`);
  }
+
+
+function pow (base, power) {
+       if(typeof base !== 'number' || typeof power !== 'number') {
+              throw new TypeError('Base and Power must be a number');
+       } 
+       if (power <= 0) {
+              throw new RangeError('Power must be more than 0');
+       }
+       let res = 1;
+       for (let i = 0; i < power; i++) {
+             res *= base;
+       }
+       return res;
+}
