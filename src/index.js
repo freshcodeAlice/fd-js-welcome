da7b6dca2b828ad6c534d6f454df592369f0b13f
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-13 17:00:56 +0300

    Anagramma task

--- src/index.js
+++ src/index.js
@@ -20,3 +20,45 @@ function translater(str, vocabulary) {
 }
 
 
+/*
+
+
+
+
+*/
+
+
+const user1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 20,
+    email: 'test@mail1'
+}
+
+const user2 = {
+    firstName: 'Jane',
+    lastName: 'Snow',
+    age: 25,
+    email: 'test2@mail2'
+}
+
+
+const johnMessages = ['hello', 'how are u', 'go to walk'];
+const janeMessage = ['hi','Im fine', 'Go!'];
+
+const mapMessage = new Map();
+mapMessage.set(user1, johnMessages);
+mapMessage.set(user2, janeMessage);
+
+
+
+/*
+Написати функцію, яка перевіряє, чи можна відтворити рядок за допомогою іншого рядка.
+
+Функція приймає два рядки, і перевіряє, чи можна з літер одного рядка створити інший.
+
+compare('test', 'sett') //true
+compare('love', 'evol') //true
+compare('test', 'love') // false
+
+*/
