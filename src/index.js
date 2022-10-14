5b4da508d3bf4db2dafd5ca9cb1c0fd8194eeb23
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-14 17:00:48 +0300

    Array desctruction and other species

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-
-/*
-перетворити меп у ['1 - johndoe', '2 - janedoe', '3 - alexdoe']
-
-const map = new Map([[1, {username: 'john doe'}], [2, {username: 'janedoe'}], [3, {username: 'alexdoe'}]]);
-
-
-[...map.entries()].map((cortege)=>{
-    return `${cortege[0]} ${cortege[1].username}`
-})
-
-*/
-
-
 /*
 const userObj = {
     name: {
@@ -100,3 +86,63 @@ const userObj = {
 function getFullName({name: {first, last, ...restOfName}, ...restOfUser}) {
     return `${first} ${last}`
 }
+
+
+///////////////////////
+
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+const [firstElem, secondElem,,,fiveElement] = arr;
+
+const [first, ...restOfArray] = arr;
+
+function getStringOfCortege([first, second]) {
+    return `${first} ${second}`
+}
+
+
+
+/*
+перетворити меп у ['1 - johndoe', '2 - janedoe', '3 - alexdoe']
+
+const map = new Map([[1, {username: 'john doe'}], [2, {username: 'janedoe'}], [3, {username: 'alexdoe'}]]);
+
+
+[...map.entries()].map((cortege)=>{
+    return `${cortege[0]} ${cortege[1].username}`
+}) 
+
+-----> 
+
+[...map.entries()].map(([key, {username}])=>{
+    return `${key} ${username}`
+})
+
+*/
+
+
+
+const settings = {
+    width: 200,
+    height: 400
+}
+
+function getSet({width, height, on = false}){
+    console.log(`${width} ${height} ${on}`)
+}
+
+const us = {
+    name: {
+        first: 'Ricky'
+    },
+    pass: '1234'
+}
+
+
+const {
+    name: {
+        first: userFirstName, 
+        last: userLastName = 'Anonumus'
+    }, 
+    pass = 'qwerty'} = us;
